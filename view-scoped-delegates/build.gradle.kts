8e8aac639fca2a29059e9c8e0f0886e0003bd92d
buszi0809 <kacper.wojciechowski08090@gmail.com>
2022-11-22 21:32:47 +0100

    Migrate gradle to kts

---
+++ view-scoped-delegates/build.gradle.kts
@@ -0,0 +1,44 @@
+import com.kwdev.buildSrc.AndroidVersion
+import com.kwdev.buildSrc.Libs
+
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = AndroidVersion.compileSdk
+    buildToolsVersion = AndroidVersion.buildTools
+
+    defaultConfig {
+        minSdk = AndroidVersion.minSdk
+        targetSdk = AndroidVersion.targetSdk
+
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(Libs.AndroidX.fragment)
+    implementation(Libs.AndroidX.lifecycle)
+}
